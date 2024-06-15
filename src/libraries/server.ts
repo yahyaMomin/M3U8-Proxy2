9ecdaaee96024bfbefe20652710e13621299b928
Josh Holly <joshholly@gmail.com>
2024-06-14 19:00:39 -0500

    Fixed CORS on /src/libraries/server.ts

--- src/libraries/server.ts
+++ src/libraries/server.ts
@@ -663,11 +663,14 @@ export async function proxyTs(url: string, headers: any, req, res: http.ServerRe
         },
     };
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    
     // Proxy request and pipe to client
     try {
         if (forceHTTPS) {
             const proxy = https.request(options, (r) => {
                 r.headers["content-type"] = "video/mp2t";
+                r.headers["Access-Control-Allow-Origin"] = "*";
                 res.writeHead(r.statusCode ?? 200, r.headers);
 
                 r.pipe(res, {
